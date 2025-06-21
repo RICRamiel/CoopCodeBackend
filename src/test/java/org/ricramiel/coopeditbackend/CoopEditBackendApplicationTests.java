950c6681799bf49e6ebac381640420412dac3653
RICRamiel <tardis40type@gmail.com>
2025-06-21 14:25:39 +0700

    init

---
+++ src/test/java/org/ricramiel/coopeditbackend/CoopEditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ricramiel.coopeditbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopEditBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
